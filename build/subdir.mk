67145c2c647a260b21b1ed44463f586986dbd9ef
Dirk Grappendorf <dirk@grappendorf.net>
2014-09-25 19:05:50 +0200

    Add makefiles

---
+++ build/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../fischerbot.cpp 
+
+OBJS += \
+./fischerbot.o 
+
+CPP_DEPS += \
+./fischerbot.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR C++ Compiler'
+	avr-g++ -I"/home/grappendorf/workspace-arduino/arduino-eclipse" -Wall -Os -fpack-struct -fshort-enums -ffunction-sections -fdata-sections -funsigned-char -funsigned-bitfields -fno-exceptions -mmcu=atmega1284p -DF_CPU=16000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
